8d8be6af799548e2343c77e9fcd54f9c1678b0e6
ARTHUR KLUG JUNIOR <arthur.junior@alunos.sc.senac.br>
2024-04-15 19:57:23 -0300

    Criado input de data com o datePicker

--- app/src/main/java/com/senac/teladenovaviagem/MainActivity.kt
+++ app/src/main/java/com/senac/teladenovaviagem/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.senac.teladenovaviagem
 
+import android.app.DatePickerDialog
+import android.icu.util.GregorianCalendar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -14,14 +20,23 @@ import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.senac.teladenovaviagem.components.MyTopBar
 import com.senac.teladenovaviagem.ui.theme.TelaDeNovaViagemTheme
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,20 +57,30 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Myapp(){
-    Scaffold (
+fun Myapp() {
+    Scaffold(
         topBar = {
             MyTopBar()
         }
-    ){
+    ) {
+
+        var showDatePickerDialog = remember {
+            mutableStateOf(false)
+        }
+        var selectedDate = remember {
+            mutableStateOf("")
+        }
+
+        val datePickerState = rememberDatePickerState()
+
         Column(
             modifier = Modifier
                 .padding(it)
                 .padding(16.dp)
         ) {
-            Row (
+            Row(
                 verticalAlignment = Alignment.CenterVertically
-            ){
+            ) {
                 Text(
                     text = "Destino",
                     fontSize = 22.sp,
@@ -84,9 +109,9 @@ fun Myapp(){
                 )
             }
 
-            Row (
+            Row(
                 verticalAlignment = Alignment.CenterVertically
-            ){
+            ) {
 
                 RadioButton(
                     selected = true,
@@ -120,7 +145,68 @@ fun Myapp(){
                 )
             }
 
+            Row {
+
+                Text(
+                    text = "Data Inicio",
+                    fontSize = 22.sp,
+                    textAlign = TextAlign.Left,
+                    modifier = Modifier
+                        .weight(1.5f)
+                        .padding(top = 16.dp)
+                )
+            }
+
+            Row {
+
+                if (showDatePickerDialog.value) {
+                    DatePickerDialog(
+                        onDismissRequest = { showDatePickerDialog.value = false },
+                        confirmButton = {
+                            Button(
+                                onClick = {
+                                    datePickerState
+                                        .selectedDateMillis?.let { millis ->
+                                            selectedDate.value = millis.toBrazilianDateFormat()
+                                        }
+                                    showDatePickerDialog.value = false
+                                }) {
+                                Text(text = "Escolher data")
+                            }
+                        }) {
+                        DatePicker(state = datePickerState)
+                    }
+                }
+
+                OutlinedTextField(
+                    value = selectedDate.value,
+                    onValueChange = { },
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth()
+                        .onFocusChanged {
+                            if (it.isFocused) {
+                                showDatePickerDialog.value = true
+                            }
+                        },
+                    readOnly = true
+                )
+
+            }
+
         }
 
     }
+}
+
+fun Long.toBrazilianDateFormat(
+    pattern: String = "dd/MM/yyyy"
+): String {
+    val date = Date(this)
+    val formatter = SimpleDateFormat(
+        pattern, Locale("pt-br")
+    ).apply {
+        timeZone = TimeZone.getTimeZone("GMT")
+    }
+    return formatter.format(date)
 }
